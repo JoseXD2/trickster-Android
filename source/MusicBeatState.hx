47e5eb309d7989416a172c296ca55594381df65a
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-09-09 14:51:53 -0300

    Update MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -91,15 +91,6 @@ class MusicBeatState extends FlxUIState
 	}
 	override function create()
 	{
-		
-		if (transIn != null)
-			trace('reg ' + transIn.region);
-
-		super.create();
-	}
-
-	override function create()
-	{
 		(cast (Lib.current.getChildAt(0), Main)).setFPSCap(FlxG.save.data.fpsCap);
 
 		if (transIn != null)
